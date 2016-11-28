b2f5757a0eeda3225e18a254c721f80c2dc9fc5a
Valeriy <v.lanemets@qiwi.ru>
2016-11-29 02:38:19 +0300

    [BF] fix authorization

--- src/main/java/my/homework/security/SecurityConfiguration.java
+++ src/main/java/my/homework/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package my.homework.security;
 import my.homework.country.CountryCodeResolver;
 import my.homework.settings.ThrottlingRequestSettings;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -31,15 +32,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .antMatchers("/apply", "/loans_approved/**")
-            .authenticated()
-            .and()
-            .httpBasic()
-            .and()
-            .authorizeRequests()
-            .antMatchers("/h2-console/**")
-            .permitAll();
+
+        http.addFilterAfter(throttlingRequestFilter, BasicAuthenticationFilter.class)
+            .antMatcher("/apply");
+
+        http.authorizeRequests().antMatchers("/**").authenticated().and().httpBasic();
 
         http.csrf().disable();
         http.headers().frameOptions().disable();
@@ -48,6 +45,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-        http.addFilterAfter(throttlingRequestFilter, BasicAuthenticationFilter.class);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
     }
 }
